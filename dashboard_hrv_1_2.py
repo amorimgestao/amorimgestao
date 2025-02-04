9d3d31f8a7ca1f2aff981169a36d554ed92018c1
amorimgestao <amorimgestaohrv@gmail.com>
2025-02-03 21:44:44 -0300

    Update dashboard_hrv_1_2.py

--- dashboard_hrv_1_2.py
+++ dashboard_hrv_1_2.py
@@ -52,7 +52,7 @@ st.markdown("""
         }
         /* Reduz o espaçamento entre o título e o st.metric */
         [data-testid="stMetric"] {
-            margin-top: -25px;
+            margin-top: -40px;
         }
     </style>
 """, unsafe_allow_html=True)
